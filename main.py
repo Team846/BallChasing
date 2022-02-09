ae473785775d1612eb27f5234078c610403bd364
KeshavSoni17 <49103006+KeshavSoni17@users.noreply.github.com>
2022-02-09 15:44:23 -0800

    small fixes

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 
 depth = angle = 0
 
-hsv_values = [0, 157, 42, 255, 255, 255]#[57, 181, 22, 146, 306, 120]
+hsv_values = [0, 0, 0, 255, 255, 255]
 
 #NetworkTables.initialize(server='192.168.0.10')
 #table = NetworkTables.getTable('SmartDashboard')
@@ -41,21 +41,25 @@ def code():
             imageR = image[:, :width//2]
             imageL = image[:, width//2:]
 
-            posR, r, imageRT = track.ball(imageR)
-            posL, r, imageLT = track.ball(imageL)
+            posR, _, imageRT = track.ball(imageR)
+            posL, _, imageLT = track.ball(imageL)
 
             depth = find_distance(imageRT, imageLT, posR, posL)
             angle = find_angle(90, height, width/2, posR, posL)
 
-            true_x = depth*math.sin(angle * math.pi/180)
-            true_y = depth*math.cos(angle * math.pi/180)
+            #true_x = depth*math.sin(angle * math.pi/180)
+            #true_y = depth*math.cos(angle * math.pi/180)
 
             #table.putNumber('distance_to_ball', depth)
             #table.putNumber('angle_to_ball', angle)
+            
+            #display_image = imageR #color
+            display_image = imageRT #mask
+            #display_image = cv2.bitwise_and(imageR, imageR, mask= imageRT) #mask + color
 
-            #display_image = draw.circle(cv2.bitwise_and(imageR, imageR, mask= imageRT), posR, 10)
-            display_image = draw.circle(imageRT, posR, 10)
-            display_image = cv2.resize(display_image, (int(1344/4), int(376/2)))
+            display_image = draw.dot(display_image, posR, 5)
+            #display_image = cv2.resize(display_image, (int(1344/2), int(376)))
+
             yield (b'--frame\r\n' 
                 b'Content-type: text/plain\r\n\r\n' + cv2.imencode('.jpg', display_image)[1].tostring() + b'\r\n')
 
